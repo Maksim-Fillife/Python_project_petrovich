682e2f4a1b41ff52870edc237e9bc0ac7a51f8a9
MaksimFilatov <Fillife.one@ya.ru>
2025-09-24 09:49:25 +0300

    Add environment data

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ dotenv.load_dotenv()
 
 password = os.getenv("PASSWORD")
 email = os.getenv("EMAIL")
-invalid_password = ("<PASSWORD>")
+invalid_password = "<PASSWORD>"
 BASE_URL = "https://petrovich.ru/"
