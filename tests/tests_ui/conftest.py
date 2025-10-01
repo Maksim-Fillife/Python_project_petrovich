a0973368a857bc4d3c2d2618d7bd44264553a379
MaksimFilatov <Fillife.one@ya.ru>
2025-10-01 13:37:40 +0300

    add fixtures: cart_page, delivery_page, services_page

--- tests/tests_ui/conftest.py
+++ tests/tests_ui/conftest.py
@@ -1,8 +1,11 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
+from pages.sevices_page import ServicesPage
 from pages.product_page import ProductPage
+from pages.delivery_page import DeliveryPage
 from pages.login_page import LoginPage
+from pages.cart_page import CartPage
 from pages.main_page import MainPage
 from selenium import webdriver
 import pytest
@@ -15,7 +18,7 @@ def driver():
     options = webdriver.ChromeOptions()
     # options.add_argument('--headless')
     # options.add_argument('--no-sandbox')
-    options.add_argument('--incognito')
+    # options.add_argument('--incognito')
     options.add_argument('--window-size=1920,1080')
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36")
     options.add_argument("--disable-blink-features=AutomationControlled")
@@ -36,3 +39,15 @@ def login_page(driver):
 @pytest.fixture
 def product_page(driver):
     return ProductPage(driver)
+
+@pytest.fixture
+def cart_page(driver):
+    return CartPage(driver)
+
+@pytest.fixture
+def delivery_page(driver):
+    return DeliveryPage(driver)
+
+@pytest.fixture
+def services_page(driver):
+    return ServicesPage(driver)
