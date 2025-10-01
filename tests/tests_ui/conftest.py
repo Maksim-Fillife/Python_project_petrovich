510d3ee69ac0e5bb7ed452792335493d0eb9ba68
MaksimFilatov <Fillife.one@ya.ru>
2025-10-01 14:35:56 +0300

    Refactoring

--- tests/tests_ui/conftest.py
+++ tests/tests_ui/conftest.py
@@ -1,14 +1,17 @@
+import pytest
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-from pages.sevices_page import ServicesPage
+from selenium import webdriver
+
+from pages.services_page import ServicesPage
 from pages.product_page import ProductPage
 from pages.delivery_page import DeliveryPage
 from pages.login_page import LoginPage
 from pages.cart_page import CartPage
 from pages.main_page import MainPage
-from selenium import webdriver
-import pytest
+
+
 
 
 
@@ -18,7 +21,7 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    # options.add_argument('--incognito')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36")
     options.add_argument("--disable-blink-features=AutomationControlled")
