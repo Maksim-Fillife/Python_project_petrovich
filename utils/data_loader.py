ce420293064c87ddae7afa73cdc7a8bb9941a57d
MaksimFilatov <Fillife.one@ya.ru>
2025-10-10 12:15:17 +0300

    refactor

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,7 +1,11 @@
+from pathlib import Path
 import random
 
+DATA_DIR = Path(__file__).parent.parent / "data"
+
 def load_all_product_code():
-    with open('../../data/product_code.txt', 'r', encoding='utf8') as file:
+    codes = DATA_DIR / "product_codes.txt"
+    with open(codes, encoding='utf8') as file:
         codes = [line.strip() for line in file]
     return codes
 
